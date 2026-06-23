6ecdeeae4ec70046d26bfdb39aeb38ef18e00b8e
Hafash Group <hafashgroup60@gmail.com>
2026-06-23 20:11:05 +0000

    I have copied the real Firebase configuration from my Firebase Console.

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -5,10 +5,10 @@
  * These values are synchronized with your Firebase project 'hafash-pk'.
  */
 export const firebaseConfig = {
-  apiKey: "AIzaSyC7z9Xf_Placeholder_Real_Key_From_Tool",
+  apiKey: "AIzaSyBBh-aekSYcl0_3mHXtodGe3z9Ek-qCE2k",
   authDomain: "hafash-pk.firebaseapp.com",
   projectId: "hafash-pk",
   storageBucket: "hafash-pk.firebasestorage.app",
-  messagingSenderId: "123456789012",
-  appId: "1:123456789012:web:abcdef1234567890"
+  messagingSenderId: "609016296952",
+  appId: "1:609016296952:web:0c28e65443e34d785ed20d"
 };
