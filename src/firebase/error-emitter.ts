657f650fbcfb711b08f2fc3bef8cc25d45e90e82
Hafash Group <hafashgroup60@gmail.com>
2026-07-11 10:27:54 +0000

    The Next.js configuration has already been corrected.
    Do not modify nex

--- src/firebase/error-emitter.ts
+++ src/firebase/error-emitter.ts
@@ -1,7 +1,28 @@
 'use client';
 
-import { EventEmitter } from 'events';
+/**
+ * A browser-compatible event emitter implementation.
+ * Replaces the Node.js 'events' module which is not available in Client Components.
+ */
+class ErrorEmitter {
+  private listeners: { [event: string]: Function[] } = {};
 
-class ErrorEmitter extends EventEmitter {}
+  on(event: string, listener: Function) {
+    if (!this.listeners[event]) {
+      this.listeners[event] = [];
+    }
+    this.listeners[event].push(listener);
+  }
+
+  off(event: string, listener: Function) {
+    if (!this.listeners[event]) return;
+    this.listeners[event] = this.listeners[event].filter(l => l !== listener);
+  }
+
+  emit(event: string, ...args: any[]) {
+    if (!this.listeners[event]) return;
+    this.listeners[event].forEach(listener => listener(...args));
+  }
+}
 
 export const errorEmitter = new ErrorEmitter();
